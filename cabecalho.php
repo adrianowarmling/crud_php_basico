dca593d9af4c12c9b7688d7841e502ba808fab98
adrianowarmling <adrianowarmling@unidavi.edu.br>
2018-03-07 19:40:08 -0300

    addphp

---
+++ cabecalho.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minha Loja</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/loja.css" />   
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("cabecalho.php");?>
+    
+    <h1>BEM VINDO</h1>
+
+<?PHP include_once("rodape.php");?>
